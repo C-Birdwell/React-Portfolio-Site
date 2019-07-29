b131e69dba000e82066155df401c4c84d5adf2e8
Colin Birdwell <Colin.birdwell@paymentdata.com>
2019-07-29 16:11:16 -0500

    image pathing

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-
 const Header = () => (
   <header>
-    <h1>Expensify</h1>
+    <img src="/assets/images/logo.svg" />
+    <h1>Web By Birdwell</h1>
     <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
     <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
   </header>
