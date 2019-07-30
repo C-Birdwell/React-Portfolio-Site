fd6d367d35c3173fb58b68b4ee355a05a4ea7706
Colin Birdwell <Colin.birdwell@paymentdata.com>
2019-07-29 20:52:10 -0500

    Added styles and layout of Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,19 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 const Header = () => (
   <header>
+    <div className="row">
+    <div className="col-1">
     <img src="/assets/images/logo.svg" />
     <h1>Web By Birdwell</h1>
+    </div>
+    <div className="col-2">
     <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
     <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
+    </div>
+    </div>
+    
+    
+    
   </header>
 );
 
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -11,15 +11,19 @@ import Header from "../components/Header";
 const AppRouter = () => (
   <BrowserRouter>
     <div>
-      <Header />
-      <Switch>
-        <Route path="/" component={DashboardPage} exact={true} />
-        <Route path="/expensedash" component={ExpenseDashboardPage} />
-        <Route path="/create" component={AddExpensePage} />
-        <Route path="/edit/:id" component={EditExpensePage} />
-        <Route path="/help" component={HelpPage} />
-        <Route component={NotFoundPage} />
-      </Switch>
+      <div className="header-container">
+        <Header />
+      </div>
+      <div className="content-container">
+        <Switch>
+          <Route path="/" component={DashboardPage} exact={true} />
+          <Route path="/expensedash" component={ExpenseDashboardPage} />
+          <Route path="/create" component={AddExpensePage} />
+          <Route path="/edit/:id" component={EditExpensePage} />
+          <Route path="/help" component={HelpPage} />
+          <Route component={NotFoundPage} />
+        </Switch>
+      </div>
     </div>
   </BrowserRouter>
 );
