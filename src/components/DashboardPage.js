410f6fc771511de99bd9e0f348571b4496d61abd
Colin Birdwell <Colin.birdwell@paymentdata.com>
2019-08-05 22:02:23 -0500

    Added Logo icon and animation library.

--- src/components/DashboardPage.js
+++ src/components/DashboardPage.js
@@ -1,9 +1,41 @@
-import React from "react";
+import React from 'react';
+import { connect } from 'react-redux';
+import Anime from 'react-anime';
+import { duration } from 'moment';
 
-const DashboardPage = () => (
-  <div>
-    <p>Hello World!</p>
-  </div>
-);
 
-export default DashboardPage;
+export class DashboardPage extends React.Component {
+  render() {
+
+    const animeProps = {
+      opacity: [0, 1],
+      translateY: [250, 0],
+      delay: 1250,
+      duration: 2000
+    }
+
+    return (
+      <Anime {...animeProps}>
+  <p>Blog Post</p>
+  <p>Blog Post</p>
+
+  <section>
+    <p>Upon this, Daggoo, with either hand upon the gunwale to steady his way, swiftly slid aft, and then erecting himself volunteered his lofty shoulders for a pedestal.</p>
+    <p>"Good a mast-head as any, sir. Will you mount?"</p>
+    <p>"That I will, and thank ye very much, my fine fellow; only I wish you fifty feet taller."</p>
+  </section>
+</Anime>
+    );
+  }
+};
+
+const mapStateToProps = (state, props) => ({
+
+});
+
+const mapDispatchToProps = (dispatch, props) => ({
+
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(DashboardPage);
+
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+
+const renderName = () => (
+  <h1>
+    <span>Web By</span>
+    <span>Birdwell</span>
+  </h1>
+)
+
+const renderLogo = () => (
+  <div className="row">
+      <div className="col-1 center">
+        <i className="icon-logo"></i>
+      </div>
+        <div className="col-4">
+          {renderName()}
+        </div>
+      </div>
+)
+
 const Header = () => (
   <header>
     <div className="row">
     <div className="col-1">
-    <img src="/assets/images/logo.svg" />
-    <h1>Web By Birdwell</h1>
+        {renderLogo()}
     </div>
     <div className="col-2">
-    <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
-    <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
+      <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
+      <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
     </div>
     </div>
     
