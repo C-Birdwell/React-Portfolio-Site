f096c1c06632054a692df5b5abaa2ee9df4f964c
Colin Birdwell <Colin.birdwell@paymentdata.com>
2020-10-28 17:49:35 -0500

    updated server info

--- server/server.js
+++ server/server.js
@@ -1,15 +1,17 @@
-const path = require('path');
-const express = require('express');
-const app = express();
-const publicPath = path.join(__dirname, '..', 'public');
-const port = process.env.PORT || 3000;
+const path = require('path')
+const express = require('express')
+const app = express()
+const publicPath = path.join(__dirname, '..', 'public')
+const port = process.env.PORT || 3000
+const sslRedirect = require('heroku-ssl-redirect')
 
-app.use(express.static(publicPath));
+app.use(express.static(publicPath))
+app.use(sslRedirect())
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(publicPath, 'index.html'));
-});
+  res.sendFile(path.join(publicPath, 'index.html'))
+})
 
 app.listen(port, () => {
-  console.log('Server is up!');
-});
+  console.log('Server is up!')
+})
