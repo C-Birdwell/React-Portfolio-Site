6a7c716fc8798973258aaf8c637840b77978b85b
Colin Birdwell <Colin.birdwell@paymentdata.com>
2020-10-28 18:03:01 -0500

    removed heroku-ssl-redirect

--- server/server.js
+++ server/server.js
@@ -3,10 +3,8 @@ const express = require('express')
 const app = express()
 const publicPath = path.join(__dirname, '..', 'public')
 const port = process.env.PORT || 3000
-const sslRedirect = require('heroku-ssl-redirect').default
 
 app.use(express.static(publicPath))
-app.use(sslRedirect())
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(publicPath, 'index.html'))
