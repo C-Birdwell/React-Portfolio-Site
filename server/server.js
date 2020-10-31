fe03c593c859819c1f8ea0c39996eee2ca169d89
Colin Birdwell <Colin.birdwell@paymentdata.com>
2020-10-31 17:54:46 -0500

    https test

--- server/server.js
+++ server/server.js
@@ -1,9 +1,14 @@
 const path = require('path')
 const express = require('express')
+// const http = require('http')
+// const enforce = require('express-sslify')
+var sslRedirect = require('heroku-ssl-redirect')
+
 const app = express()
 const publicPath = path.join(__dirname, '..', 'public')
 const port = process.env.PORT || 3000
 
+app.use(sslRedirect)
 app.use(express.static(publicPath))
 
 app.get('*', (req, res) => {
